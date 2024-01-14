96110dcf85c095fe49279e27507ad696f75f25f0
Aaro Sulonen <sulonea2@aalto.fi>
2024-01-14 15:25:18 +0200

    2.11

--- part2/puhelinluettelo/src/App.jsx
+++ part2/puhelinluettelo/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 
 const PersonForm = ({ persons, setPersons, newName, setNewName, newNumber, setNewNumber }) => {
@@ -65,16 +66,22 @@ const Phonebook = ({persons, filter}) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setNewFilter] = useState('')
 
+  useEffect( () => {
+    console.log('effect')
+
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response =>{
+        console.log('promise done', response)
+        setPersons(response.data)
+        })
+    
+  }, [])
   
 
   

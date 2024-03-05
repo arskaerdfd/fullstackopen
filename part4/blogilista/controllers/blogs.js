99d7bab970a1f6f704112a1943ed702578a55f3a
Aaro Sulonen <sulonea2@aalto.fi>
2024-03-05 16:42:48 +0200

    4.13

--- part4/blogilista/controllers/blogs.js
+++ part4/blogilista/controllers/blogs.js
@@ -3,16 +3,23 @@ const blogsRouter = require('express').Router()
 
 
 
-blogsRouter.get('/', (request, response) => {
+blogsRouter.get('/', async (request, response) => {
   
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response) => {
+blogsRouter.get('/:id', async (request, response) => {
+
+  try {
+    const blog = await Blog.findById(request.params.id)
+    response.json(blog)
+  } catch {
+    response.status(404).json({error: "Blog not found"})
+  }
+})
+
+blogsRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
 
   if (!blog.likes) {
@@ -23,11 +30,17 @@ blogsRouter.post('/', (request, response) => {
     return response.status(400).json({error: "Title or url missing"})
   }
   
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
+  const result = await blog.save()
+  response.status(201).json(result)
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+  try {
+    await Blog.findByIdAndDelete(request.params.id)
+    response.status(204).end()
+  } catch {
+    response.status(404).end()
+  }
 })
 
 module.exports = blogsRouter

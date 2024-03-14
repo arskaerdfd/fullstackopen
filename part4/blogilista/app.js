64d3746fae73da9154425ac436be0b68e17ff8fe
Aaro Sulonen <sulonea2@aalto.fi>
2024-03-14 20:31:45 +0200

    4.20

--- part4/blogilista/app.js
+++ part4/blogilista/app.js
@@ -7,12 +7,14 @@ const notesRouter = require('./controllers/blogs')
 const config = require('./utils/config')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
+const middleware = require('./utils/middleware')
 
 const mongoUrl = config.MONGODB_URL
 mongoose.connect(mongoUrl)
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.tokenExtractor)
 
 app.use('/api/blogs', notesRouter)
 app.use('/api/users', usersRouter)

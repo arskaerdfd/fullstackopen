64ab327d91ba3b5e91c7a2c73ba7e862c6140c99
Aaro Sulonen <sulonea2@aalto.fi>
2024-04-07 15:26:04 +0300

    5.14

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('Title gets rendered', async () => {
@@ -11,11 +12,35 @@ test('Title gets rendered', async () => {
     __v: 0
   }
 
-  render(<Blog blog={blog} />)
+  const { container } = render(<Blog blog={blog} />)
+  expect(container).toHaveTextContent('Type wars')
+  expect(container).not.toHaveTextContent('http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html')
+  expect(container).not.toHaveTextContent('2')
+})
+
+test('Likes and url shown when button is clicked', async () => {
+  const blog = {
+    _id: '5a422bc61b54a676234d17fc',
+    title: 'Type wars',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+    likes: 2,
+    __v: 0
+  }
+
+  const mockHandler = vi.fn()
+
+  const { container } = render(<Blog blog={blog} />)
+
+
+  const user = userEvent.setup()
+  const button = screen.getByText('view')
+  await user.click(button)
 
   screen.debug()
 
-  const element = screen.getByText(/Type wars/)
+  expect(container).toHaveTextContent('2')
+  expect(container).toHaveTextContent('http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html')
 
-  expect(element).toBeDefined()
 })
+
